9a33b9746a50a557e9346e2135c71a3e5d737b79
Daiki Anjiki <daikianjiki@gmail.com>
2023-05-18 15:54:22 -0700

    edited

--- ArraysAndStrings/isPermutation.java
+++ ArraysAndStrings/isPermutation.java
@@ -2,7 +2,7 @@ package ArraysAndStrings;
 
 import java.util.Arrays;
 
-public class IsPermutation {
+public class isPermutation {
 
     // Check Permutation: Given two strings, write a method to decide if one 
     // is a permutation of the other.
